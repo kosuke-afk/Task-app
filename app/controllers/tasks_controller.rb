2ed7d2b911167b75402bad6278ad73a0ffd298af
EC2 Default User <ec2-user@ip-172-31-25-218.ap-northeast-1.compute.internal>
2020-11-12 04:23:42 +0000

    createできた　一応

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,34 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.where(user_id: params[:user_id])
-  end
   
-  def new
-    @task = Task.find(params[:user_id])
-  end
+ def show
+   @task = Task.find(params[:user_id])
+ end
+ def index
+   @tasks = Task.where(user_id: params[:user_id])
+ end
   
-  def create
-    @task = Task.new(params_task)
-    if @task.save
-      flash[:success] = "新規タスクを作成しました。"
-      redirect_to @task
-    else
-      render :new
-    end
-  end
+ def new
+  @task = Task.new
+ end
   
-  def edit
-  end
-  
-  private
-   
-   def params_task
-     params.require(:task).permit(:name,:description)
+ def create
+   @user = User.find(params[:user_id])
+   @task = Task.new(task_params)
+   if @task.save
+     flash[:success] = "新規タスクを作成しました。"
+     redirect_to user_tasks_url(@user)
+   else
+     render :new
    end
+ end
+  
+ def edit
+ end
+  
+   private
+   
+     def task_params
+        params.require(:task).permit(:name, :description).merge(user_id: params[:user_id])
+     end
   
 end
