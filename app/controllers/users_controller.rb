4d9198e80ae3c559e74e8a29d5c5e334051c0185
EC2 Default User <ec2-user@ip-172-31-25-218.ap-northeast-1.compute.internal>
2020-11-13 04:59:55 +0000

    c11　Taskのバリデーションエラ〜メッセージの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user
-  before_action :set_task
   
   def show
   end
 
   def new
+    @user = User.new
   end
   
   def create
@@ -21,14 +20,7 @@ class UsersController < ApplicationController
   
   private
   
-    def set_user
-      @user = User.find(params[:id])
-    end
-    
-    def set_task
-      @task = Task.find(params[:id])
-    end
-    
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
